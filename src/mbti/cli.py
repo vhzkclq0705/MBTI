34fffdb56e5e9a4f2e31b2a3d6f31f97cc3a5006
vhzkclq0705 <vhzkclq0705@naver.com>
2025-01-29 16:45:12 +0900

    csv 파일 불러오기 테스트

--- src/mbti/cli.py
+++ src/mbti/cli.py
@@ -1,4 +1,5 @@
 import typer
+import os
 
 types = {
     "ISTP": "백과사전형",
@@ -18,6 +19,24 @@ types = {
     "ENFJ": "언변능숙형",
     "ENTJ": "지도자형"
 }
+
+# 나라 별 mbit 비율 확인
+# 
+# parameter
+# :country: 나라 명(영문)
+def check_country_mbti_ratio(country: str):
+    
+    # 현재 파일(cli.py)의 디렉토리 경로 (src/mbti)
+    # 절대 경로로 불러오기 위해 다음과 같이 경로 설정
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # 프로젝트 루트 기준으로 data/countries.csv 찾기
+    csv_path = os.path.join(cur_dir, "..", "..", "data", "countries.csv")
+
+    # csv 파일 불러오기
+    df = pd.read_csv(csv_path)
+
+    print(df)
 
 def check_type(mbti: str):
     return types[mbti]
